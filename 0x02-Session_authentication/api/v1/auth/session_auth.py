4472dec5d0d3120c7cefe7bc2be8b7ac3ac4825b
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-01 11:54:34 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,10 +13,15 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """ Creates a session ID for a user_id
         """
-        if user_id is None:
-            return None
-        if type(user_id) != str:
+        if user_id is None or type(user_id) != str:
             return None
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User id based on a session id
+        """
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
