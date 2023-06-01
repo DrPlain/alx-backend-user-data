60e456ece289cded75a042aced61def081e44e80
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-01 10:37:25 +0100

    task 0

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from api.v1.auth.auth import Auth
+""" Module for SessionAuth class
+"""
+
+
+class SessionAuth(Auth):
+    """ class SessionAuth for session authentication
+    """
+    pass
