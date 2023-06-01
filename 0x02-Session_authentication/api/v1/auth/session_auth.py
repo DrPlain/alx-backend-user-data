53d80b72ca17d6b32de390a6daff5ae056820101
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-01 13:52:17 +0100

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,12 +28,12 @@ class SessionAuth(Auth):
             return None
         return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None):
         """ Returns a user instance based on a cookie value
         """
         session_id = self.session_cookie(request)
         if session_id is None:
             return None
         user_id = self.user_id_for_session_id(session_id)
-        User.get(user_id)
-        return
+        if user_id:
+            return User.get(user_id)
