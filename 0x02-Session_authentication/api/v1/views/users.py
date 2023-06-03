8266f9deaa4df448c2a619c9c0c6eb0fb30a6a94
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-03 08:45:08 +0100

    Edit task 1

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -124,7 +124,7 @@ def update_user(user_id: str = None) -> str:
     return jsonify(user.to_json()), 200
 
 
-@app_views.route('/users/me')
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False))
 def get_auth_user():
     """ Gets authenticated user
     """
