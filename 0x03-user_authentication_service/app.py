2665886b2ddfd95da0228f02c75b54b0b7d1a88f
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-06 16:27:38 +0100

    User registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Basic flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,28 @@ def index():
     """
     payload = {"message": "Bienvenue"}
     return jsonify(payload)
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """ User registration
+    """
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = None
+        try:
+            user = AUTH.register_user(email, password)
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
+        except Exception as err:
+            pass
+        if user:
+            payload = {
+                "email": email,
+                "message": "user created"
+            }
+            return jsonify(payload)
 
 
 if __name__ == "__main__":
