098a8532fdbc0fdecb6c5fe55207514a5fa5f09f
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-06 10:51:26 +0100

    Type annoted add_user function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,8 +34,10 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds a user to the database
+        Return: User object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
         return user
