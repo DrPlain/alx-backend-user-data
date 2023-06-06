a766a48c50b957c42a6d40962e8ae33878b4b712
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-06 11:28:16 +0100

    Implemented find_user_by method of DB

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from typing import TypeVar
-
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -39,5 +39,24 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user by a key-value pair
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        columns = User.__table__.columns.keys()
+
+        for key in kwargs.keys():
+            if key not in columns:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
 
         return user
