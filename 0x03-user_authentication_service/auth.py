4d0f3176e17380ffb37cb1d44dbbd072ccaaf3c0
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-07 12:53:16 +0100

    Implemented find user by session_id in Auth model

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,16 @@ class Auth:
             session_id = str(uuid4())
             user.session_id = session_id
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Find user by session_id
+        """
+        if not session_id:
+            return None
+
+        user = None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
+        return user
